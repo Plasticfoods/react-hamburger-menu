66f6d53d9d635b5f2b4503e045a04605d87ed9f8
Rajdeep <rajdeep.hascode@gmail.com>
2024-02-15 22:08:09 +0530

    hamburger-menu shifted to right

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,12 +1,19 @@
 import navItems from "@/navItems";
 import { Link } from "react-router-dom";
-
+import Logo from "./Logo";
+import { Chrome } from 'lucide-react';
 
 export default function NavMobile() {
     let navItemBgColor = '#09090B';
 
     return (
-        <nav className='nav-mobile' style={{ backgroundColor: navItemBgColor }}>
+        <nav className='nav-mobile ' style={{ backgroundColor: navItemBgColor }}>
+            <div className="">
+                <Link to='/' className='flex gap-2 items-center'>
+                    <Chrome size={20} />
+                    <div className='font-bold' style={{ fontSize: '1.2rem' }}>randomUI</div>
+                </Link>
+            </div>
             <ul>
                 {navItems.map((navItem) => {
                     return (
